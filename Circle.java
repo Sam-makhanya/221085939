db302c1a2cd6de8f3796219dd0f3af867d86dc05
Sam-makhanya <101555488+Sam-makhanya@users.noreply.github.com>
2022-04-21 11:33:45 +0200

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,69 @@
+package za.ac.wsu.s221085939;
+/*Created by samkelo makhanya
+ * Date created 20/04/2022
+ * calculating circle Area, Circumference, Diameter
+ */
+	public class Circle{
+
+	  private float radius;
+	  private int x;
+	  private int y;
+
+	public Circle(float radius,int x,int y){
+
+	this.x=x;
+	this.y=y;
+	this.radius=radius;
+
+	}
+	public void setX(int x){
+	   this.x=x;
+	}
+
+	public void setY(int y){
+	   this.y=y;
+	}
+
+	public void setRadius(float radius){
+		
+	  if(radius>0) {
+	   this.radius=radius;
+	  }
+	  else {
+		  System.out.println("Radius is must be positive ");
+	  }
+	}
+	public float getRadius(){
+	    return radius;
+	}
+
+	public int getX(){
+	   return x;
+	}
+
+	public int getY(){
+	    return y;
+	}
+
+	void calculateArea(){
+		   System.out.println("X is : "+getX());
+		   System.out.println("Y is : "+getY());
+		   System.out.println("Radius is: "+getRadius());
+		  
+	   System.out.println("Area : "+Math.PI*(getRadius()*getRadius()));
+	  
+	}
+
+	void calculateCircumference(){
+
+	  System.out.println("Circumference : "+2*Math.PI*radius);
+
+	}
+
+
+	void calculateDiameter(){
+
+	 System.out.println("Diameter : "+getRadius()+getRadius());
+
+	 }
+}
---
+++ CircleApp.java
@@ -0,0 +1,19 @@
+
+	package za.ac.wsu.s221085939; 
+
+	public class CircleApp{
+
+	public static void main(String[] args) {
+
+	 Circle object = new Circle(5,4,6);
+	 
+
+	 object.getX();
+	 object.getY();
+	 object.getRadius();
+	 object.calculateArea();
+	 object.calculateCircumference();
+	 object.calculateDiameter();
+
+	}
+}
